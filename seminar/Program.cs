77b5d33992ba02ce1699251add62f909abea2385
Valera <leo.plv@bk.ru>
2022-12-05 11:11:23 +0500

    семинар3.2

--- seminar/Program.cs
+++ seminar/Program.cs
@@ -1,16 +1,61 @@
-﻿void Quarters(int x,int y)
+﻿//void Quarters(int x,int y)
+//{
+//    if(x==0||y==0)
+//    Console.WriteLine("x=0 or y=0");
+//    else if(x>0 && y>0)
+//    Console.WriteLine("I");
+//    else if(x<0 && y>0)
+//    Console.WriteLine("II");
+//    else if(x<0 && y<0)
+//    Console.WriteLine("III");
+//    else if(x<0 && y>0)
+//    Console.WriteLine("IV");
+//}
+
+//Quarters(-10,9);
+
+
+
+
+//void Quarters(string a)
+//{
+//    if(a=="I")
+//    Console.WriteLine("x>0 or y>0");
+//    else if(a=="II")
+//    Console.WriteLine("x<0 or y>0");
+//    else if(a=="III")
+//    Console.WriteLine("x<0 or y<0");
+//    else if(a=="IV")
+// /   Console.WriteLine("x>0 or y<0");
+//    else 
+//    Console.WriteLine("error");
+//}
+
+//Quarters(Console.ReadLine());
+
+
+int ReadNumber(string message)
 {
-    if(x==0||y==0)
-    Console.WriteLine("x=0 or y=0");
-    else if(x>0 && y>0)
-    Console.WriteLine("I");
-    else if(x<0 && y>0)
-    Console.WriteLine("II");
-    else if(x<0 && y<0)
-    Console.WriteLine("III");
-    else if(x<0 && y>0)
-    Console.WriteLine("IV");
+    Console.WriteLine(message);
+    var s=Console.ReadLine();
+    int a =s ==null? 0:int.Parse(s);
+    return a;
+}
+double Distance(int ax,int bx,int ay,int by)
+{
+    double result;
+    result=Math.Sqrt(Math.Pow(bx-ax,2)+Math.Pow(by-ay,2));
+    return result;
 }
 
-Quarters(-10,9);
 
+
+
+
+
+int ax=ReadNumber("Введите X:");
+int ay=ReadNumber("Введите Y:");
+int bx=ReadNumber("Введите X:");
+int by=ReadNumber("Введите Y:");
+
+Console.WriteLine(Math.Round(Distance(ax,ay,bx,by)));
