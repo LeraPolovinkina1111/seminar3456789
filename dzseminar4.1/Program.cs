6ce28280108db590797d33c4b20c5599ed140629
Valera <leo.plv@bk.ru>
2022-12-08 16:16:29 +0500

    дз4/1

---
+++ dzseminar4.1/Program.cs
@@ -0,0 +1,21 @@
+﻿void Degree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+
+int Enter(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int numberA = Enter("Введите число A: ");
+int numberB = Enter("Введите число B: ");
+Degree(numberA, numberB);
