c7d43119e90ee98a537110212611c7c4c55d11f0
Valera <leo.plv@bk.ru>
2022-12-12 14:38:19 +0500

    дз5.2

---
+++ dzseminar5.3/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+double[] MassNums(int size, int from, int to)
+{
+    double[] arr = new double[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+
+
+void KolneChetNum(int[] arr)
+{
+    int nechet;
+    nechet = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+
+            arr[i]++;
+        }
+
+        else
+        {
+            nechet = nechet + arr[i];
+        }
+
+    }
+    Console.WriteLine($"Kolich nechet: {nechet}");
+}
+
+double[] arr_1 = MassNums(int.Parse(Console.ReadLine()),
+                       int.Parse(Console.ReadLine()),
+                       int.Parse(Console.ReadLine()));
+Print(arr_1);
+KolneChetNum(arr_1);
