edfcb2de86188c41df2d1f4b3747d5249d9349c8
Valera <leo.plv@bk.ru>
2022-12-14 16:34:30 +0500

    4

---
+++ seminar4/Program.cs
@@ -0,0 +1,63 @@
+﻿//int SumNums(int num)
+//{
+//    int all_sum =0;
+//   for(int i=1;i<=num;i++)
+// {
+//     all_sum+=i;
+// }
+
+//return all_sum;
+
+//}
+
+//Console.WriteLine(SumNums(int.Parse(Console.ReadLine())));
+
+//int i=0;
+//i++;
+
+
+
+
+//int Numcount(int n)
+//{
+//  int count=0;
+//while(n>0)
+//{
+//    n=n/10;
+//    count++;
+//
+
+
+//}
+//return count;
+//}
+//Console.WriteLine(Numcount(int.Parse(Console.ReadLine())));
+
+
+
+
+
+//int Proiz(int num)
+//{
+//    int all_sum = 1;
+//   for (int i = 1; i <= num; i++)
+// {
+//    all_sum = all_sum * i;
+//
+//  }
+//return all_sum;
+//}
+//Console.WriteLine(Proiz(int.Parse(Console.ReadLine())));
+
+
+
+
+
+
+
+
+
+
+
+
+
