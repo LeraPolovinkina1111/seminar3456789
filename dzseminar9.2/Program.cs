aeacb3cb2d6e7f5ef3ae033bf037397e43bb43bf
Valera <leo.plv@bk.ru>
2022-12-25 14:33:59 +0500

    dz9.2

---
+++ dzseminar9.2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задайте значения M и N.
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+string rec(int m, int n)
+{
+    if (m == n)
+        return $"{m} ";
+    return rec(m, n - 1) + $"{n} ";
+}
+
+Console.Clear();
+
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(rec(m, n));
